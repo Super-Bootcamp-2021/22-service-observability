f2a7d073adfcf48118ca08cb500ad1fbb774bb3f
muhammadisan <muhammadisan2015@gmail.com>
2021-02-18 23:38:35 +0700

    jaeger worker done

---
+++ tugas/service/lib/tracer.js
@@ -0,0 +1,25 @@
+const { initTracer } = require('jaeger-client');
+
+exports.createTracer = (serviceName) => {
+  const config = {
+    serviceName,
+    sampler: {  
+      type: 'const',
+      param: 1,
+    },
+    reporter: {
+      logSpans: true,
+    },
+  };
+  const options = {
+    logger: {
+      info(msg) {
+        console.log('INFO', msg);
+      },
+      error(msg) {
+        console.log('ERROR', msg);
+      },
+    },
+  };
+  return initTracer(config, options);
+}
